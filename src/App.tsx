2b94aa71a69e187a567b37cc981d8ac79a3fe777
takeike <takemasa.ikeda@konicaminolta.com>
2022-02-20 08:40:08 +0900

    58 customhook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+
+const user = {
+  id: 1,
+  name: "TOM",
+  email: "aaaa@consoto.com",
+  address: "Tokyo Harajuku"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <UserCard user={user} />
     </div>
   );
 }
